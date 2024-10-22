700174f99b0d6187151eb967200d67c7eee03e4f
pokis-01 <cs05528@uoi.gr>
2024-10-22 22:39:23 +0300

    Add files via upload

--- lab02.s
+++ lab02.s
@@ -7,11 +7,19 @@ array: .word 1, 0, 1, 12, 0, 1, 4
 
     la a0, array
     li a1, 7    # unsigned
-    li a2, 1
+    li a2, 3   
 prog:
 #-----------------------------
-# Write your code here!
-# Do not remove the prog label or write code above it!
+    add s0, zero,zero
+loop: 
+    beq a1,zero,done
+    lw t1,0(a0)
+    bne a0,t1,if
+    add s0, zero,a0
+if:
+    addi a1,a1,-1
+    addi a0,a0,4
+    j loop
 #-----------------------------
 done:
     addi a7, zero, 10 
